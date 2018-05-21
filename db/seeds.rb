18045c06b6c7f2182a907c9821ded14c0fd05d59
kalolo <kalolo@gmail.com>
2018-05-21 12:24:40 -0700

    Adding project

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Album.create({name:"Default Album", description:"Default Album"})
+#Album.create({name:"Default Album", description:"Default Album"})
